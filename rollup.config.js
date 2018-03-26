3df636dc2330df57f090e702d5ad1596041d4fa7
Rory Duncan <robust.rory@gmail.com>
2018-03-26 06:58:44 +0000

    Scaffold rollup, svelte

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+import svelte from 'rollup-plugin-svelte';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'public/bundle.js',
+    format: 'iife'
+  },
+  plugins: [
+    svelte({
+      // You can restrict which files are compiled
+      // using `include` and `exclude`
+      include: 'src/components/**/*.html',
+
+      // By default, the client-side compiler is used. You
+      // can also use the server-side rendering compiler
+      generate: 'ssr',
+
+      // Extract CSS into a separate file (recommended).
+      // See note below
+      css: function (css) {
+        // console.log(css.code); // the concatenated CSS
+        // console.log(css.map); // a sourcemap
+
+        // creates `main.css` and `main.css.map` — pass `false`
+        // as the second argument if you don't want the sourcemap
+        css.write('public/main.css');
+      }
+    })
+  ]
+}
---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+// import HellowWorld from "./components/HelloWorld"
+
+// var app = new HelloWorld({
+//   target: document.querySelector('main'),
+//   data: {
+//     name: 'world'
+//   }
+// });
